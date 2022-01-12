e38e1ed08b4d50b6457ab24b4870358ccb26f81b
g.meo <g.meo@outlook.com>
2022-01-12 13:51:36 +0100

    add package laravel filemanager

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use UniSharp\LaravelFilemanager\Lfm;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::group(['prefix' => 'laravel-filemanager', 'middleware' => 'auth'], function () {
+    Lfm::routes();
+});
 
 Route::get('/', function () {
     return view('welcome');
