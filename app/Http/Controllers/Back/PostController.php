f9dea989963d5b850731d83d37bf9bf074d265c1
g.meo <g.meo@outlook.com>
2022-01-15 21:33:49 +0100

    add delete function for article and sweat alert

--- app/Http/Controllers/Back/PostController.php
+++ app/Http/Controllers/Back/PostController.php
@@ -12,6 +12,15 @@ use App\DataTables\PostsDataTable;
 
 class PostController extends Controller
 {
+    /**
+     * Create the controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->authorizeResource(Post::class, 'post');
+    }
 
     /**
      * Display a listing of the resource.
